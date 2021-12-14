f7f9d34404d39bc05da2024edb336d29925328dd
Plotinus Respecter <94573803+PlotinusRespecter@users.noreply.github.com>
2021-12-14 18:50:14 -0500

    windown prompt robot name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScsript is running!")
+// this cratures a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself messages.");
+
+console.log(10 +10);
+
+console.log("Our robot's name is " + playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
